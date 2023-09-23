48f70b2fa379a52dff8edad05c3b45c29e706355
MarcelMarins <marcelsouza.o@gmail.com>
2023-09-22 22:53:49 -0300

    Upload dos arquivos

---
+++ MaterialDesign_MarcelMarins3RB/public_html/js/MeuJS2.js
@@ -0,0 +1,58 @@
+$(document).ready(function (){
+    alert("Meu primeiro código com Jquery...!!!") 
+});
+
+
+/*
+$(function (){
+    alert("jQuery deve ser mt lko")  
+})
+*/
+
+/*
+$(function (){
+    $("*").css("color","#0489B1");
+    $("*").css("padding","10px"); 
+    $("*").css("font-family","sans-serif");
+})
+*/
+
+/*
+$(function (){
+    $("h1").css("color","#0489B1");
+    $("a").css("color","blue");
+    $("p").css("color","green");
+})
+*/
+
+/*
+$(function (){
+    $("*").css("font-family","sans-serif");
+    $(".link02").css("color", "green");
+    $("#link03").css("color", "#8A0829");
+    $(".link04").css("color", "#FF0080");
+    $(".link05").css("color", "#DF0101");
+    $(".link06").css("color", "#64FE2E");
+})
+*/
+
+//id -> #
+//classe -> .****
+/*
+$(function (){
+    $("a").css("color", "red");
+    $(".link04").css("color", "green");
+    $("#link03").css("color", "blue");
+    $(".link05").css("font-size", "36px");
+    $("p").css({
+        color: "#8A0829",
+        "font-family":"arial",
+    })
+})
+*/
+/*para facilitar mais:
+    $("p").css({
+        color: "#8A0829",
+        fontfamily:"arial",
+    })
+*/
---
+++ MaterialDesign_MarcelMarins3RB/public_html/js/Pagina04_js.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#btn2').click(function (){
+        $('header').addClass('jumbotron-2');
+    });
+    $('#btn1').click(function(){
+        $('header').removeClass('jumbotron-2');
+    });
+});
